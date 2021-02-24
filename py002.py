458f4865781b7feaf6beb01b8fb12e8baa89cbc4
Vanderlei Paulo Bettio <vanderleipaulobettio@hotmail.com>
2021-02-24 10:09:07 -0400

    learning exercises python

---
+++ py002.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome: ')
+print('Seja bem vindo, {}!'.format(nome))
+print('Qual a sua idade?')
+idade=input('Digite sua idade: ')
+print(format(nome)+' você esta em plena forma com seus {}'.format(idade)+' anos de idade.')
---
+++ py003.py
@@ -0,0 +1,5 @@
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+s = n1 + n2
+print('A soma de {} + {} é {}'.format(n1, n2, s))
+print(type(s))
